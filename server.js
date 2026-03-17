c548ac2f843b2563b5ed86ee4658632091236912
Swaminathan PL <srinath0209@gmail.com>
2026-03-18 00:11:35 +0530

    added health-check and error-handling

--- server.js
+++ server.js
@@ -27,3 +27,16 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Server running"
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found"
+  });
+});
