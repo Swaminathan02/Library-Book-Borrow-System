81e497802d1a18a6a59edef5551ed0e6ac4c39d5
Swaminathan PL <srinath0209@gmail.com>
2026-03-17 22:05:31 +0530

    created borrow routes with APIs

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const connectDB = require("./config/db")
 const bookRoutes = require("./routes/bookRoutes");
 const memberRoutes = require("./routes/memberRoutes");
+const borrowRoutes = require("./routes/borrowRotutes");
 const app = express();
 
 connectDB();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use("/api/books", bookRoutes);
 app.use("/api/members", memberRoutes);
+app.use("/api/borrows", borrowRoutes);
 
 app.get("/", (req, res) => {
     res.send("Library API running");
